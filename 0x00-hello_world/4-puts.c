25194268e7eae63371abba76441fb7325aa94f9d
ZANEMALI <kofie.christo@gmail.com>
2023-02-19 12:24:28 -0800

    Puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,12 @@
 #include <stdio.h>
 
 /**
- * main - prints "Programming is like building a multilingual puzzle
- * puzzle, followed by a new line.
- * Return: Always 0.
+ * main - A program that prints a line with puts function
+ * Return: 0 (Success)
  */
 
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle\n");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
